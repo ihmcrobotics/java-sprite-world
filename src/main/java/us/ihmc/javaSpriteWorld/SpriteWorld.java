4f0c71aa4cb433e6241e7f7a37dea9b49e39b602
JerryPratt <jpratt@ihmc.us>
2018-05-02 16:52:57 -0500

    Formatting clean up.

--- src/main/java/us/ihmc/javaSpriteWorld/SpriteWorld.java
+++ src/main/java/us/ihmc/javaSpriteWorld/SpriteWorld.java
@@ -8,62 +8,75 @@ public class SpriteWorld
 {
    private SpriteStage spriteStage;
    private final CopyOnWriteArrayList<Sprite> sprites = new CopyOnWriteArrayList<Sprite>();
-   
+
    private double leftBorderX = -1.0, rightBorderX = 1.0;
    private double topBorderY = -1.0, bottomBorderY = 1.0;
-   
+
    private boolean worldScrollsX = false;
    private boolean worldScrollsY = false;
-   
+
    private ArrayList<SpriteCollisionGroup> spriteCollisionGroups;
+   private ArrayList<SpriteWorldMouseListener> spriteWorldMouseListeners = new ArrayList<>();
 
    public SpriteWorld()
    {
-      
+
    }
-   
+
+   public void attacheSpriteWorldMouseListener(SpriteWorldMouseListener spriteWorldMouseListener)
+   {
+      this.spriteWorldMouseListeners.add(spriteWorldMouseListener);
+   }
+
+   public ArrayList<SpriteWorldMouseListener> getSpriteWorldMouseListeners()
+   {
+      return spriteWorldMouseListeners;
+   }
+
    public void addSpriteCollisionGroup(SpriteCollisionGroup spriteCollisionGroup)
    {
       if (spriteCollisionGroups == null)
       {
          spriteCollisionGroups = new ArrayList<>();
       }
-      
+
       spriteCollisionGroups.add(spriteCollisionGroup);
    }
-   
+
    public void checkSpriteCollisions()
    {
-      if (spriteCollisionGroups == null) return;
-      
-      for (int i=0; i<spriteCollisionGroups.size(); i++)
+      if (spriteCollisionGroups == null)
+         return;
+
+      for (int i = 0; i < spriteCollisionGroups.size(); i++)
       {
          spriteCollisionGroups.get(i).doCheckCollisions();
       }
    }
-   
+
    public SpriteWorldViewer createAndDisplaySpriteWorldViewerUsingSwing(String name, SpriteWorld spriteWorld, int preferredWidth, int preferredHeight)
    {
-	   SpriteWorldViewer viewer = new SpriteWorldViewerUsingSwing(name);
-	   viewer.setPreferredSizeInPixels(preferredWidth, preferredHeight);
-	   viewer.setResizable(false);
-	   viewer.setSpriteWorld(spriteWorld);
-	   viewer.createAndDisplayWindow();
-	   
-	   return viewer;
+      SpriteWorldViewer viewer = new SpriteWorldViewerUsingSwing(name);
+      viewer.setPreferredSizeInPixels(preferredWidth, preferredHeight);
+      viewer.setResizable(false);
+      viewer.setSpriteWorld(spriteWorld);
+      viewer.createAndDisplayWindow();
+
+      return viewer;
    }
-   
-   public SpriteWorldViewerUsingJavaFX createAndDisplaySpriteWorldViewerUsingJavaFX(String name, SpriteWorld spriteWorld, int preferredWidth, int preferredHeight)
+
+   public SpriteWorldViewerUsingJavaFX createAndDisplaySpriteWorldViewerUsingJavaFX(String name, SpriteWorld spriteWorld, int preferredWidth,
+                                                                                    int preferredHeight)
    {
       SpriteWorldViewerUsingJavaFX viewer = new SpriteWorldViewerUsingJavaFX(name);
       viewer.setPreferredSizeInPixels(preferredWidth, preferredHeight);
       viewer.setResizable(false);
       viewer.setSpriteWorld(spriteWorld);
       viewer.createAndDisplayWindow();
-      
+
       return viewer;
    }
-   
+
    public boolean getWorldScrollsX()
    {
       return worldScrollsX;
@@ -83,32 +96,32 @@ public class SpriteWorld
    {
       this.worldScrollsY = worldScrollsY;
    }
-   
+
    public void addSprite(Sprite sprite)
    {
       sprites.add(sprite);
    }
-   
+
    public void setStage(SpriteStage spriteStage)
    {
       this.spriteStage = spriteStage;
    }
-   
+
    public void setLeftBorderX(double leftBorderX)
    {
       this.leftBorderX = leftBorderX;
    }
-   
+
    public void setRightBorderX(double rightBorderX)
    {
       this.rightBorderX = rightBorderX;
    }
-   
+
    public void setTopBorderY(double topBorderY)
    {
       this.topBorderY = topBorderY;
    }
-   
+
    public void setBottomBorderY(double bottomBorderY)
    {
       this.bottomBorderY = bottomBorderY;
@@ -118,7 +131,7 @@ public class SpriteWorld
    {
       return spriteStage;
    }
-   
+
    public List<Sprite> getSprites()
    {
       return sprites;
@@ -128,17 +141,17 @@ public class SpriteWorld
    {
       return leftBorderX;
    }
-   
+
    public double getTopBorderY()
    {
       return topBorderY;
    }
-   
+
    public double getRightBorderX()
    {
       return rightBorderX;
    }
-   
+
    public double getBottomBorderY()
    {
       return bottomBorderY;
@@ -148,14 +161,15 @@ public class SpriteWorld
    {
       return Math.abs(bottomBorderY - topBorderY);
    }
-   
+
    public double getWidth()
    {
       return Math.abs(rightBorderX - leftBorderX);
    }
-   
+
    public void start()
    {
-      
+
    }
+
 }
