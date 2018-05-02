7ef1318978ec823edd44fff88ee647b1b799cfe7
JerryPratt <jpratt@ihmc.us>
2018-05-02 16:52:01 -0500

    Imports and clean up formatting.

--- src/main/java/us/ihmc/javaSpriteWorld/SampleStageBackdrops.java
+++ src/main/java/us/ihmc/javaSpriteWorld/SampleStageBackdrops.java
@@ -1,32 +1,22 @@
 package us.ihmc.javaSpriteWorld;
 
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-
-import javax.imageio.ImageIO;
-
 public class SampleStageBackdrops
 {
-
    public static StageBackdrop getBackgammonBoard()
    {
       return StageBackdrop.createFromFile("sampleImages/BackgammonBoard.jpg");
    }
-   
+
    public static StageBackdrop getWhiteChessKing()
    {
       return StageBackdrop.createFromFile("sampleImages/portablejim-Chess-tile-King-300px.png");
    }
-   
+
    public static StageBackdrop getCrossHairs()
    {
-	   StageBackdrop crossHairs = StageBackdrop.createFromFile("sampleImages/Crosshairs-3456-300px.png");
-	   crossHairs.setXReferencePercent(0.50667);
-	   crossHairs.setYReferencePercent(0.50667);
-	   return crossHairs;
+      StageBackdrop crossHairs = StageBackdrop.createFromFile("sampleImages/Crosshairs-3456-300px.png");
+      crossHairs.setXReferencePercent(0.50667);
+      crossHairs.setYReferencePercent(0.50667);
+      return crossHairs;
    }
-   
-
 }
