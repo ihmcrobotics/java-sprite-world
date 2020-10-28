85b0b0f7e2ea0e669d7866a14f7244cbf0f4da63
JerryPratt <jpratt@ihmc.us>
2020-10-28 12:02:59 -0500

    Add some conditions for when to get food.

--- src/test/java/us/ihmc/javaSpriteWorld/examples/robotChallenge/behaviorTree/GetFoodBehaviorNode.java
+++ src/test/java/us/ihmc/javaSpriteWorld/examples/robotChallenge/behaviorTree/GetFoodBehaviorNode.java
@@ -1,6 +1,7 @@
 package us.ihmc.javaSpriteWorld.examples.robotChallenge.behaviorTree;
 
-import java.util.ArrayList;
+import static us.ihmc.javaSpriteWorld.examples.robotChallenge.behaviorTree.RobotBehaviorTools.bodyToWorld;
+import static us.ihmc.javaSpriteWorld.examples.robotChallenge.behaviorTree.RobotBehaviorTools.doAttractionVectorControl;
 
 import org.apache.commons.lang3.tuple.Triple;
 
@@ -9,12 +10,14 @@ import us.ihmc.euclid.tuple2D.Vector2D;
 import us.ihmc.javaSpriteWorld.examples.behaviorTree.BehaviorTreeAction;
 import us.ihmc.javaSpriteWorld.examples.behaviorTree.BehaviorTreeNodeStatus;
 
-import static us.ihmc.javaSpriteWorld.examples.robotChallenge.behaviorTree.RobotBehaviorTools.*;
-
 public class GetFoodBehaviorNode implements BehaviorTreeAction
 {
    private final RobotBehaviorSensors sensors;
    private final RobotBehaviorActuators actuators;
+
+   private final double minimumHealthToBeNotHungry = 90.0;
+   private double minimumHealthToNotBeDesparateForFarAwayFood = 50.0;
+   private double minimumDistanceForFoodToBeConsideredClose = 10.0;
 
    private double dt = 0.01;
    private double lastVelocity = 0.0; // PD controller
@@ -28,6 +31,9 @@ public class GetFoodBehaviorNode implements BehaviorTreeAction
    @Override
    public BehaviorTreeNodeStatus tick()
    {
+      if (notHungry()) return BehaviorTreeNodeStatus.SUCCESS;
+      
+      
       Triple<Integer, Point2D, Vector2D> closestFood = null;
       double closestDistance = Double.POSITIVE_INFINITY;
       for (Triple<Integer, Point2D, Vector2D> food : sensors.getLocationOfAllFoodInBodyFrame())
@@ -39,6 +45,10 @@ public class GetFoodBehaviorNode implements BehaviorTreeAction
             closestFood = food;
          }
       }
+      
+      if (notHungryEnoughToChaseDownClosestFood(closestDistance))
+         return BehaviorTreeNodeStatus.SUCCESS;
+
 
       double finalClosestDistance = closestDistance;
       Vector2D attraction = RobotBehaviorTools.fieldVector(sensors.getGlobalPosition(),
@@ -48,5 +58,15 @@ public class GetFoodBehaviorNode implements BehaviorTreeAction
       lastVelocity = doAttractionVectorControl(sensors, actuators, attraction, lastVelocity, dt);
 
       return BehaviorTreeNodeStatus.RUNNING;
+   }
+
+   private boolean notHungryEnoughToChaseDownClosestFood(double closestDistance)
+   {
+      return (sensors.getHealth() > minimumHealthToNotBeDesparateForFarAwayFood) && (closestDistance > minimumDistanceForFoodToBeConsideredClose);
+   }
+
+   private boolean notHungry()
+   {
+      return sensors.getHealth() > minimumHealthToBeNotHungry;
    }
 }
