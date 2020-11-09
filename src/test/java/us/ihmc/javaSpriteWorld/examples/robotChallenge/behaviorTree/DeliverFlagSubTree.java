0593b3a92035016d0062caf936ea49ace9647dec
Duncan Calvert <dcalvert@ihmc.us>
2020-11-09 09:46:15 -0600

    DeliverFlagSubTree and HighLevelDeciderNode.

---
+++ src/test/java/us/ihmc/javaSpriteWorld/examples/robotChallenge/behaviorTree/DeliverFlagSubTree.java
@@ -0,0 +1,9 @@
+package us.ihmc.javaSpriteWorld.examples.robotChallenge.behaviorTree;
+
+public class DeliverFlagSubTree
+{
+   public DeliverFlagSubTree(RobotBehaviorSensors sensors, RobotBehaviorActuators actuators)
+   {
+
+   }
+}
---
+++ src/test/java/us/ihmc/javaSpriteWorld/examples/robotChallenge/behaviorTree/HighLevelDeciderNode.java
@@ -0,0 +1,17 @@
+package us.ihmc.javaSpriteWorld.examples.robotChallenge.behaviorTree;
+
+import us.ihmc.javaSpriteWorld.examples.behaviorTree.BehaviorTreeAction;
+import us.ihmc.javaSpriteWorld.examples.behaviorTree.BehaviorTreeNodeStatus;
+
+public class HighLevelDeciderNode implements BehaviorTreeAction
+{
+   public HighLevelDeciderNode(Object data, RobotBehaviorActuators actuators)
+   {
+   }
+
+   @Override
+   public BehaviorTreeNodeStatus tick()
+   {
+      return null;
+   }
+}
