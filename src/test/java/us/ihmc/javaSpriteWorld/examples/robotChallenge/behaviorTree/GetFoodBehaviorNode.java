6538a4a14084dde2998ed72e40cfda3b79f714fe
Duncan Calvert <dcalvert@ihmc.us>
2020-11-17 20:48:42 -0600

    In food action, separate utility from action and reword utility calculation.

--- src/test/java/us/ihmc/javaSpriteWorld/examples/robotChallenge/behaviorTree/GetFoodBehaviorNode.java
+++ src/test/java/us/ihmc/javaSpriteWorld/examples/robotChallenge/behaviorTree/GetFoodBehaviorNode.java
@@ -7,10 +7,10 @@ import org.apache.commons.lang3.tuple.Triple;
 
 import us.ihmc.euclid.tuple2D.Point2D;
 import us.ihmc.euclid.tuple2D.Vector2D;
-import us.ihmc.javaSpriteWorld.examples.behaviorTree.BehaviorTreeAction;
 import us.ihmc.javaSpriteWorld.examples.behaviorTree.BehaviorTreeNodeStatus;
+import us.ihmc.javaSpriteWorld.examples.behaviorTree.utility.UtilityBasedAction;
 
-public class GetFoodBehaviorNode implements BehaviorTreeAction
+public class GetFoodBehaviorNode extends UtilityBasedAction
 {
    private final RobotBehaviorSensors sensors;
    private final RobotBehaviorActuators actuators;
@@ -22,10 +22,9 @@ public class GetFoodBehaviorNode implements BehaviorTreeAction
 
    private double getFoodAttractionVectorConstantLength = 10.0;
 
-   
-   private final double minimumHealthToBeNotHungry = 90.0;
-   private double minimumHealthToNotBeDesparateForFarAwayFood = 60.0;
-   private double distanceForFoodToBeConsideredFar = 3.0;
+   private final double hungerThreshold = 90.0;
+   private double desperateHungerThreshold = 60.0;
+   private double foodCloseishThreshold = 3.0;
 
    private double dt = 0.01;
    private double lastVelocity = 0.0; // PD controller
@@ -41,8 +40,16 @@ public class GetFoodBehaviorNode implements BehaviorTreeAction
    @Override
    public double evaluateUtility()
    {
-      boolean notHungry = notHungry();
+      boolean hungry = sensors.getHealth() < hungerThreshold;
+      boolean foodIsCloseish = sensors.getClosestFoodDistance() < foodCloseishThreshold;
+      boolean desperatelyHungry = sensors.getHealth() < desperateHungerThreshold;
+      double utility = (hungry && foodIsCloseish) || desperatelyHungry ? 1.0 : 0.0;
+      return utility;
+   }
 
+   @Override
+   public BehaviorTreeNodeStatus tick()
+   {
       Triple<Integer, Point2D, Vector2D> closestFood = null;
       double closestDistance = Double.POSITIVE_INFINITY;
       for (Triple<Integer, Point2D, Vector2D> food : sensors.getLocationOfAllFoodInBodyFrame())
@@ -56,8 +63,6 @@ public class GetFoodBehaviorNode implements BehaviorTreeAction
          }
       }
 
-      boolean notHungryEnoughToChaseDownClosestFood = notHungryEnoughToChaseDownClosestFood(closestDistance);
-
       double finalClosestDistance = closestDistance;
       Vector2D attraction = RobotBehaviorTools.fieldVector(sensors.getGlobalPosition(),
                                                            bodyToWorld(sensors, closestFood.getMiddle()),
@@ -65,53 +70,10 @@ public class GetFoodBehaviorNode implements BehaviorTreeAction
 
       attraction.normalize();
       attraction.scale(getFoodAttractionVectorConstantLength);
-
-      double utility = (notHungry || notHungryEnoughToChaseDownClosestFood) ? 0.0 : 1.0;
       lastVelocity = doAttractionVectorControl(sensors, hungerAction, attraction, lastVelocity, dt, accelerationGain, turnRateGain, turnRateDamping);
 
-      return utility;
-   }
-
-   @Override
-   public BehaviorTreeNodeStatus tick()
-   {
       actuators.setAcceleration(hungerAction[0]);
       actuators.setTurnRate(hungerAction[1]);
       return BehaviorTreeNodeStatus.SUCCESS;
-   }
-
-   private boolean notHungryEnoughToChaseDownClosestFood(double closestDistance)
-   {
-      return (sensors.getHealth() > minimumHealthToNotBeDesparateForFarAwayFood) && (closestDistance > distanceForFoodToBeConsideredFar);
-   }
-
-   private boolean notHungry()
-   {
-      return sensors.getHealth() > minimumHealthToBeNotHungry;
-   }
-   
-   public void setAttractionStrength(double attractionStrength)
-   {
-      this.attractionStrength = attractionStrength;
-   }
-
-   public void setAttractionGraduation(double attractionGraduation)
-   {
-      this.attractionGraduation = attractionGraduation;
-   }
-
-   public void setAccelerationGain(double accelerationGain)
-   {
-      this.accelerationGain = accelerationGain;
-   }
-
-   public void setTurnRateGain(double turnRateGain)
-   {
-      this.turnRateGain = turnRateGain;
-   }
-
-   public void setTurnRateDamping(double turnRateDamping)
-   {
-      this.turnRateDamping = turnRateDamping;
    }
 }
