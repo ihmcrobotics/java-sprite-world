f2cc930181c24d4011d29472ca8e0c373d847c60
Sylvain Bertrand <sbertrand@ihmc.us>
2020-12-01 22:33:47 -0600

    :arrow_up: euclid 0.15.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,6 @@ mainDependencies {
 }
 
 testDependencies {
-   api("us.ihmc:euclid:0.15.1")
-   api("us.ihmc:euclid-geometry:0.15.1")
+   api("us.ihmc:euclid:0.15.2")
+   api("us.ihmc:euclid-geometry:0.15.2")
 }
