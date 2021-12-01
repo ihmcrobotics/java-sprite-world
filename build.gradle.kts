400e6af4a12bc3ccfc588ad4e14671908e88a9ec
jpratt <jerry.pratt@boardwalkrobotics.com>
2021-12-01 17:21:26 -0600

    :bookmark: 0.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 ihmc {
    group = "us.ihmc"
-   version = "0.0.2"
+   version = "0.0.3"
    vcsUrl = "https://stash.ihmc.us/projects/ICSL/repos/javaspriteworld"
    openSource = false
 
